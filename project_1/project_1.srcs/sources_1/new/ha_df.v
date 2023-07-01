f993c3438bd1d22cabd36a50e9f1f2a8d6d663b3
ROSHAN TEJA KOVILAPATI <76561059+Roshanteja21@users.noreply.github.com>
2023-07-01 11:40:05 +0530

    Add files via upload

---
+++ project_1/project_1.srcs/sources_1/new/ha_df.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.01.2022 15:21:03
+// Design Name: 
+// Module Name: ha_df
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ha_df(
+    input a,b,
+    input s,cout
+    );
+endmodule
